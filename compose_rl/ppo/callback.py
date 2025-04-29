d59a63baecdadc7444b85edc39101c6237441bd6
Abhay Gupta <gupta-abhay@users.noreply.github.com>
2025-04-29 13:17:04 -0700

    Enable different KL estimators for training (#44)
    - As name suggests
    - The definitions are taken from here:
    http://joschu.net/blog/kl-approx.html

--- compose_rl/ppo/callback.py
+++ compose_rl/ppo/callback.py
@@ -72,6 +72,7 @@ def env_generate(
     generation_kwargs: dict,
     tokenizer: Tokenizer,
     eos_token_ids: list[int],
+    kl_estimator: str,
 ) -> tuple[dict[str, torch.Tensor],
            list[tuple[str, str]],
            ReferenceOutput,
@@ -93,6 +94,7 @@ def env_generate(
         generation_kwargs (dict): Generation keyword arguments.
         tokenizer (Tokenizer): The actor critic's tokenizer.
         eos_token_ids (list[int]): A list of eos token ids.
+        kl_estimator (str): Which kl estimator to use. Options are 'k1', 'k2', 'k3' and 'k3_offpolicy'.
 
     Returns:
         partial_env_output (dict[str, tensor]): Partially complete dictionary of return elements suitable
@@ -295,6 +297,7 @@ def env_generate(
                 actions=actions,
                 action_log_probs=device_train_microbatch_log_probs,
                 device_train_microbatch_size=device_train_microbatch_size,
+                kl_estimator=kl_estimator,
                 verified_answers=verified_answers,
             )
 
@@ -326,6 +329,13 @@ class PPOCallback(CallbackWithConfig):
         self.gamma = var_config.get('gamma', 1.0)
         # Value used in the generalized advantage estimate calculation.
         self.lambda_gae = var_config.get('lambda_gae', 1.0)
+        # Which kl estimator to use
+        self.kl_estimator = var_config.get('kl_estimator', 'k1')
+        if self.kl_estimator not in ['k1', 'k2', 'k3', 'k3_offpolicy']:
+            raise ValueError(
+                f'Invalid kl estimator: {self.kl_estimator}. ' +
+                'Valid options are: k1, k2, k3, k3_offpolicy.',
+            )
 
         # Generation keyword arguments.
         self.generation_kwargs = var_config.get('generation_kwargs')
@@ -437,11 +447,6 @@ class PPOCallback(CallbackWithConfig):
         self.pad_token_idx = state.model.tokenizer.pad_token_id  # type: ignore
         self.actor_critic = state.model
 
-        if self.pad_token_idx in self.eos_token_ids:
-            log.warning(
-                'pad_token_id is in eos_token_ids list. Be careful with any data processing going forward!',
-            )
-
         # TODO (#158): do this through composer.
         for destination in ensure_tuple(logger.destinations):
             if isinstance(destination, WandBLogger):
@@ -496,7 +501,6 @@ class PPOCallback(CallbackWithConfig):
         # This needs to be done here becuase callbacks are init'd before we attach
         # the dataloader as a property to state
         self.tokenizer = state.model.tokenizer
-
         self.eos_token_ids = [self.tokenizer.eos_token_id]  # type: ignore
         if self.input_eos_token_ids is not None:
             self.eos_token_ids = self.input_eos_token_ids
@@ -507,6 +511,11 @@ class PPOCallback(CallbackWithConfig):
                 log.info(
                     f'Token {eos_token_id} is {self.tokenizer.decode([eos_token_id])}.',  # type: ignore
                 )
+
+        if self.pad_token_idx in self.eos_token_ids:
+            log.warning(
+                'pad_token_id is in eos_token_ids list. Be careful with any data processing going forward!',
+            )
 
         self.train_prompt_loader_iter = iter(
             self.train_prompt_loader,  # pyright: ignore
@@ -660,6 +669,7 @@ class PPOCallback(CallbackWithConfig):
                 generation_kwargs=self.generation_kwargs,
                 tokenizer=self.tokenizer,  # type: ignore
                 eos_token_ids=self.eos_token_ids,  # type: ignore
+                kl_estimator=self.kl_estimator,
             )
 
             self.prompts_and_gens.extend(prompts_and_gens)
